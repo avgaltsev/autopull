92fd52b77bae438cc03f47f60b969a15673ccaa8
avgaltsev <avgaltsev@gmail.com>
2013-05-06 21:55:43 +0400

    readRepos moved to index controller

--- autopull.py
+++ autopull.py
@@ -1,9 +1,11 @@
 import os, subprocess, json
+
 from ConfigParser import ConfigParser
+
 from bottle import get, post, request, run, default_app
 
 
-def readRepos(filename='repos.ini'):
+def readRepos(filename="repos.ini"):
     
     config = ConfigParser()
     config.read(filename)
@@ -22,48 +24,48 @@ def readRepos(filename='repos.ini'):
     return repos
 
 
-def pull(path, remote='origin', branch='master'):
+def pull(path, remote="origin", branch="master"):
     
-    subprocess.call(['git', '--git-dir=' + path + '/.git/', '--work-tree=' + path, 'checkout', branch])
-    #subprocess.call(['git', '--git-dir=' + path + '/.git/', '--work-tree=' + path, 'pull', remote, branch]) # Updates working tree fine, but doesn't update remote branch ref.
-    subprocess.call(['git', '--git-dir=' + path + '/.git/', '--work-tree=' + path, 'fetch', remote])
-    subprocess.call(['git', '--git-dir=' + path + '/.git/', '--work-tree=' + path, 'merge', remote + '/' + branch])
+    subprocess.call(["git", "--git-dir=" + path + "/.git/", "--work-tree=" + path, "checkout", branch])
+    #subprocess.call(["git", "--git-dir=" + path + "/.git/", "--work-tree=" + path, "pull", remote, branch]) # Updates working tree fine, but doesn"t update remote branch ref.
+    subprocess.call(["git", "--git-dir=" + path + "/.git/", "--work-tree=" + path, "fetch", remote])
+    subprocess.call(["git", "--git-dir=" + path + "/.git/", "--work-tree=" + path, "merge", remote + "/" + branch])
 
 
-@get('/')
+@get("/")
 def getIndex():
     
     pass
 
 
-@post('/')
+@post("/")
 def postIndex():
     
-    raw = request.forms.get('payload')
+    repos = readRepos();
+    
+    raw = request.forms.get("payload")
     
     data = json.loads(raw) if isinstance(raw, str) else {}
     
-    if ('repository' in data) and ('name' in data['repository']) and (data['repository']['name'] in repos):
+    if ("repository" in data) and ("name" in data["repository"]) and (data["repository"]["name"] in repos):
         
-        repo = repos[data['repository']['name']]
+        repo = repos[data["repository"]["name"]]
         
-        if ('path' in repo) and (os.path.isdir(repo['path'])):
+        if ("path" in repo) and (os.path.isdir(repo["path"])):
             
             kwargs = {}
             
-            if 'remote' in repo:
-                kwargs['remote'] = repo['remote']
+            if "remote" in repo:
+                kwargs["remote"] = repo["remote"]
             
-            if 'branch' in repo:
-                kwargs['branch'] = repo['branch']
+            if "branch" in repo:
+                kwargs["branch"] = repo["branch"]
             
-            pull(repo['path'], **kwargs)
+            pull(repo["path"], **kwargs)
 
 
-repos = readRepos();
-
-
-if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=8080)
+    
 else:
     application = default_app()
